22ba86b64b41836be659645fd075900aaa9b8469
sangmin <sang93423@gmail.com>
2018-06-11 13:55:16 +0900

    장고프로젝트 초기세팅, admin계정 생성까지 완료

--- education/config/urls.py
+++ education/config/urls.py
@@ -13,29 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.db import models
-from django.conf import settings
-from django.utils import timezone
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path, include
 
-# 클래스가 데이터베이스처럼 동작하게 하고싶을때 사용
-
-
-class Post(models.Model):
-    author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-    )
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(
-        default=timezone.now)
-    published_date = models.DateTimeField(
-        blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-# 객채를 문자열로 추가하고 싶을때 __str__사용
-    def __str__(self):
-        return self.title
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'', include('blog.urls'))
+]
